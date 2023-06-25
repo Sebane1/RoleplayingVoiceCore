1c2ce41d67f34148d78181e200a4416a3beb0f12
Sebastina <sebane1@thavea.com>
2023-06-25 14:06:40 -0500

    Initial Commit

---
+++ RoleplayingVoiceManager.cs
@@ -0,0 +1,19 @@
+﻿using ElevenLabs;
+using System.Diagnostics.Contracts;
+
+namespace RoleplayingVoiceCore {
+    public class RoleplayingVoiceManager {
+        private ElevenLabsClient _api;
+
+        public RoleplayingVoiceManager(string apiKey) {
+            _api = new ElevenLabsClient(apiKey);
+        }
+
+        public async Task<string> GetVoice(string text, string apikey, string voiceType) {
+            var voice = (await _api.VoicesEndpoint.GetAllVoicesAsync()).FirstOrDefault();
+            var defaultVoiceSettings = await _api.VoicesEndpoint.GetDefaultVoiceSettingsAsync();
+            var clipPath = await _api.TextToSpeechEndpoint.TextToSpeechAsync(text, voice, defaultVoiceSettings);
+            return clipPath;
+        }
+    }
+}
