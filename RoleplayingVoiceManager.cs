79f89524b322821967d2728338cc9f321ee318ae
Sebastina <sebane1@thavea.com>
2023-06-28 23:17:30 -0500

    Add volume adjustement.

--- RoleplayingVoiceManager.cs
+++ RoleplayingVoiceManager.cs
@@ -53,7 +53,8 @@ namespace RoleplayingVoiceCore {
                         CharacterVoices.VoiceCatalogue[voiceType] = new Dictionary<string, string>();
                     }
                     if (!CharacterVoices.VoiceCatalogue[(voiceType)].ContainsKey(trimmedText.ToLower())) {
-                        clipPath = await _api.TextToSpeechEndpoint.TextToSpeechAsync(@"""" + trimmedText.Replace(@"""", null) + @"""", characterVoice,
+                        clipPath = await _api.TextToSpeechEndpoint
+                            .TextToSpeechAsync(@"""" + trimmedText.Replace(@"""", null) + @"""", characterVoice,
                             defaultVoiceSettings, null,
                             Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\RPVoiceCache");
                         CharacterVoices.VoiceCatalogue[(voiceType)].Add(trimmedText.ToLower(), clipPath);
@@ -61,7 +62,8 @@ namespace RoleplayingVoiceCore {
                         clipPath = CharacterVoices.VoiceCatalogue[(voiceType)][trimmedText.ToLower()];
                     } else {
                         CharacterVoices.VoiceCatalogue[(voiceType)].Remove(trimmedText.ToLower());
-                        clipPath = await _api.TextToSpeechEndpoint.TextToSpeechAsync(@"""" + trimmedText.Replace(@"""", null) + @"""", characterVoice,
+                        clipPath = await _api.TextToSpeechEndpoint
+                            .TextToSpeechAsync(@"""" + trimmedText.Replace(@"""", null) + @"""", characterVoice,
                             defaultVoiceSettings, null,
                             Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\RPVoiceCache");
                         CharacterVoices.VoiceCatalogue[(voiceType)].Add(trimmedText.ToLower(), clipPath);
@@ -115,27 +117,28 @@ namespace RoleplayingVoiceCore {
                 byte[] hashBytes = md5.ComputeHash(inputBytes);
 
                 return Convert.ToHexString(hashBytes); // .NET 5 +
-
-                // Convert the byte array to hexadecimal string prior to .NET 5
-                // StringBuilder sb = new System.Text.StringBuilder();
-                // for (int i = 0; i < hashBytes.Length; i++)
-                // {
-                //     sb.Append(hashBytes[i].ToString("X2"));
-                // }
-                // return sb.ToString();
             }
         }
-        public async Task<string> GetVoice(string sender, string text) {
+        public async Task<string> GetVoice(string sender, string text, float volume) {
             if (_networkedClient != null) {
-                string path = await _networkedClient.GetFile(CreateMD5(sender + text),
-                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\RPVoiceCache");
+                string path = "";
+                string hash = CreateMD5(sender + text);
+                string localPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\RPVoiceCache", hash + ".mp3");
+                if (!File.Exists(localPath)) {
+                    path = await _networkedClient.GetFile(hash,
+                        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\RPVoiceCache");
+                } else {
+                    path = localPath;
+                }
                 if (!string.IsNullOrEmpty(path)) {
                     WaveOutEvent output = new WaveOutEvent();
                     using (var player = new AudioFileReader(path)) {
+                        output.Volume = Math.Clamp(volume, 0, 1);
                         output.Init(player);
                         output.Play();
                     }
                 }
+
             }
             return "";
         }
